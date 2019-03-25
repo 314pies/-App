d6890be182b00260b44bb57fd43d59fed70fe21f
DESKTOP-ESM69JQ\ubnnjh <ian121363@gmail.com>
2019-03-25 21:55:51 +0800

    Update root folder

---
+++ hataraku-saibou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hataraku-saibou
+//
+//  Created by ShiFayChy on 2018/10/3.
+//  Copyright © 2018年 LienChen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
